1cdeadcf795ff68b9febd4cbc62140106d36082f
Henrique Ioneda <henriqueioneda@gmail.com>
2017-03-09 21:33:38 -0300

    adding second step of wicked

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  acc_types.each do |acc_type|
+  self.acc_types.each_key do |acc_type|
     define_method("#{acc_type}?") do
-      self.acc_type == acc_types[acc_type.to_sym]
+      self.acc_type == self.class.acc_types[acc_type.to_sym]
     end
   end
 
