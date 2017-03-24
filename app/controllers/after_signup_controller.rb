d288d8d478ab1122f89961efedeea571273b38ae
Henrique Ioneda <henriqueioneda@gmail.com>
2017-03-24 09:03:29 -0300

    fixes to after_signup

--- app/controllers/after_signup_controller.rb
+++ app/controllers/after_signup_controller.rb
@@ -12,7 +12,8 @@ class AfterSignupController < ApplicationController
     def update
         @user = current_user
         if params[:user]
-            @user.assign_attributes(params.require(:user).permit(:acc_type, :cc_number, :cc_name, :cc_exp, :cpf_cpnj))
+            p = params.require(:user).permit(:acc_type, :cc_number, :cc_name, :cc_exp, :cpf_cpnj, :bank_agency, :bank_account)
+            @user.assign_attributes(p)
         end
         bypass_sign_in(@user)
         render_wizard @user
